a6d0700d09d64f8f0c75ce6e95219ed1d996fee9
MaxYashch <maxyashch@gmail.com>
2021-03-07 20:34:31 +0300

    start project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { React } from "react";
+import { Header } from "./layout/Header";
+
+
+function App() {
+  return (
+    // <React.Fragment>
+    //   <Header />
+    // <React.Fragment />
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
